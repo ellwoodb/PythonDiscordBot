be56840ec8af4dd068982d3a0dc4629e4da69bd9
ellwoodb <56759474+matthew230506@users.noreply.github.com>
2020-12-02 20:47:31 +0100

    removed logging

--- bot/cogs/logging.py
+++ bot/cogs/logging.py
@@ -15,14 +15,14 @@ class Logging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_member_update(before, after):
-        if len(before.roles) < len(after.roles):
-            # The user has gained a new role, so lets find out which one
-            newRole = next(
-                role for role in after.roles if role not in before.roles)
+    # @commands.Cog.listener()
+    # async def on_member_update(before, after):
+    #     if len(before.roles) < len(after.roles):
+    #         # The user has gained a new role, so lets find out which one
+    #         newRole = next(
+    #             role for role in after.roles if role not in before.roles)
 
-            print(f"User gained {newRole.name}")
+    #         print(f"User gained {newRole.name}")
 
 
 def setup(bot):
