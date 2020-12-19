0590de72a8b2ef9c58a47fbd91c4ac78be0b118f
ellwoodb <56759474+matthew230506@users.noreply.github.com>
2020-12-19 12:17:04 +0100

    Slash command test

---
+++ bot/cogs/slash.py
@@ -0,0 +1,27 @@
+# Name: slash.py
+# Kind: Slash Cog
+# Version: 0.0.1
+
+### For testing only ###
+
+import discord
+from discord.ext import commands
+import data.embeds
+from data.embeds import embeds
+from discord.ext.commands.errors import *
+from discord_slash import SlashCommand
+from discord_slash import SlashContext
+
+
+class Slash(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.slash = SlashCommand(bot, override_type=True)
+
+        @self.slash.slash(name="ping")
+        async def ping_slash(ctx):
+            await ctx.send(content=f"Pong! (`{round(bot.latency*1000)}`ms)")
+
+
+def setup(bot):
+    bot.add_cog(Slash(bot))
